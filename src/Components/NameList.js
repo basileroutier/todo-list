f8b7f16c198826fd5a211241c3c39c3bd819785f
ian cotton <55019@etu.he2b.be>
2023-01-19 18:46:06 +0100

    Y en a qui bosse

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -3,8 +3,11 @@ import React from 'react'
 function NameList() {
 
     const names = ['Jackie', 'Titouan', 'Théodule']
+    const NameList = names.map(name => <h2>{name}</h2>)
 
   return (
-    <div>NameList</div>
+    <div>
+         {NameList }
+    </div>
   )
 }
